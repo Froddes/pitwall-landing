6389ecf64b0b59e81a4e9825ba4453f90001aac1
Frarodmar19 <frarodmar19@alum.us.es>
2026-09-14 17:03:58 +0200

    update politics

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // privacidad: resolve(__dirname, 'privacidad.html'),
+        privacidad: resolve(__dirname, 'politica-privacidad.html'),
+        aviso: resolve(__dirname, 'aviso-legal.html'),
+        cookies: resolve(__dirname, 'politica-cookies.html'),
       }
     }
   }
